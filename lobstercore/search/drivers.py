d0c2135a0b9d5ff904d785b5ea7cb68273bac489
David Reed <david.reedfrancis@sainsburys.co.uk>
2020-02-18 20:17:18 +0000

    Fixes config

--- lobstercore/search/drivers.py
+++ lobstercore/search/drivers.py
@@ -1,150 +1,152 @@
-import json, datetime, ConfigParser, os
+import datetime
+import ConfigParser
+import json
+import os
+
+import app
+
 from abc import ABCMeta, abstractmethod, abstractproperty
 from time import mktime
 from sqlalchemy import event
 from elasticsearch import Elasticsearch
 from lobstercore.models import Content
 
+
 class Driver:
-  __metaclass__ = ABCMeta
-  config = {}
+    __metaclass__ = ABCMeta
+    config = {}
 
-  def __init__(self, config):
-    self.config = config
+    def __init__(self, config):
+        self.config = config
 
-  @abstractmethod
-  def connect(self):
-    pass
+    @abstractmethod
+    def connect(self):
+        pass
 
-  @abstractmethod
-  def drop_index(self):
-    pass
+    @abstractmethod
+    def drop_index(self):
+        pass
 
-  @abstractmethod
-  def create_index(self):
-    pass
+    @abstractmethod
+    def create_index(self):
+        pass
 
-  @abstractmethod
-  def reindex(self, db_session):
-    pass
+    @abstractmethod
+    def reindex(self, db_session):
+        pass
 
-  @abstractmethod
-  def update(self, mapper, connection, target):
-    pass
+    @abstractmethod
+    def update(self, mapper, connection, target):
+        pass
 
-  @abstractmethod
-  def delete(self, mapper, connection, target):
-    pass
+    @abstractmethod
+    def delete(self, mapper, connection, target):
+        pass
 
-  @abstractmethod
-  def search(self, doc_type, query):
-    pass
+    @abstractmethod
+    def search(self, doc_type, query):
+        pass
+
 
 class ElasticsearchDriver(Driver):
-  __connection__ = False
+    __connection__ = False
 
-  def connect(self):
-    if not self.__connection__:
-       self.__connection__ = Elasticsearch(self.config['endpoint'], request_timeout=15)
-    return self.__connection__
+    def connect(self):
+        if not self.__connection__:
+            self.__connection__ = Elasticsearch(
+                self.config["endpoint"], request_timeout=15
+            )
+        return self.__connection__
 
-  def drop_index(self):
-    es = self.connect()
-    if es.indices.exists(index = self.config['index']):
-      es.indices.delete(index = self.config['index'])
+    def drop_index(self):
+        es = self.connect()
+        es.indices.delete(index=self.config["index"])
+        pass
 
-  def create_index(self):
-    es = self.connect()
-    es.indices.create(index = self.config['index'])
+    def create_index(self):
+        es = self.connect()
+        es.indices.create(index=self.config["index"])
+        pass
 
-  def reindex(self, db_session):
-    es = self.connect()
-    contents = db_session.query(Content).order_by(Content.id)
-    for content in contents:
-      es.index(
-        index = self.config['index'],
-        doc_type = content.__name__.lower(),
-        body = content.to_dict(),
-        id = content.id
-      )
+    def reindex(self, db_session):
+        es = self.connect()
+        contents = db_session.query(Content).order_by(Content.id)
+        for content in contents:
+            es.index(
+                index=self.config["index"],
+                doc_type=content.__name__,
+                body=content.to_dict(),
+                id=content.id,
+            )
 
-  def update(self, mapper, connection, target):
-    es = self.connect()
-    es.index(
-      index = self.config['index'],
-      doc_type = target.__name__.lower(),
-      body = target.to_dict(),
-      id = target.id
-    )
+    def update(self, mapper, connection, target):
+        es = self.connect()
+        es.index(
+            index=self.config["index"],
+            doc_type=target.__name__,
+            body=target.to_dict(),
+            id=target.id,
+        )
 
-  def delete(self, mapper, connection, target):
-    es = self.connect()
-    es.delete(
-      index = self.config['index'],
-      doc_type = target.__name__.lower(),
-      id = target.id
-    )
+    def delete(self, mapper, connection, target):
+        es = self.connect()
+        es.delete(index=self.config["index"], doc_type=target.__name__, id=target.id)
 
-  def search(self, doc_type, query):
-    es = self.connect()
-    results = es.search(
-      index = self.config['index'],
-      doc_type = doc_type.lower(),
-      q = query
-    )
-    resultset = []
-    if results['hits']['total'] == 0:
-      return resultset
+    def search(self, doc_type, query):
+        es = self.connect()
+        results = es.search(
+            index=self.config["index"], doc_type=doc_type.lower(), q=query
+        )
+        resultset = []
+        if results["hits"]["total"] == 0:
+            return resultset
 
-    for result in results['hits']['hits']:
-      resultset.append(result['_source'])
+        for result in results["hits"]["hits"]:
+            resultset.append(result["_source"])
 
-    return resultset
+        return resultset
+
 
 def register():
-  event.listen(Content, 'after_insert', dispatch_update)
-  event.listen(Content, 'after_update', dispatch_update)
-  event.listen(Content, 'after_delete', dispatch_delete)
+    event.listen(Content, "after_insert", dispatch_update)
+    event.listen(Content, "after_update", dispatch_update)
+    event.listen(Content, "after_delete", dispatch_delete)
+
 
 def get_driver():
-  driver = False
-  config = ConfigParser.ConfigParser()
-  config.read('/etc/lobstercms.conf')
-  driver_config = dict(config.items('search'))
+    driver = False
 
-  if config.get('search', 'enabled', False) is False:
+    if app.config["search"]["driver"] == "elasticsearch":
+        driver = ElasticsearchDriver(dict(app.config["search"]))
+
     return driver
 
-  if config.get('search', 'driver') == 'solr':
-    driver = SolrDriver(driver_config);
-
-  if config.get('search', 'driver') == 'elasticsearch':
-    driver = ElasticsearchDriver(driver_config);
-
-  return driver
 
 def dispatch_update(mapper, connection, target):
-  driver = get_driver()
-  if driver is False:
-    return
-  driver.update(mapper, connection, target)
+    driver = get_driver()
+    if driver is False:
+        return
+    driver.update(mapper, connection, target)
+
 
 def dispatch_delete(mapper, connection, target):
-  driver = get_driver()
-  if driver is False:
-    return
-  driver.delete(mapper, connection, target)
+    driver = get_driver()
+    if driver is False:
+        return
+    driver.delete(mapper, connection, target)
+
 
 def initialise(db_session):
-  driver = get_driver()
-  if driver is False:
-    return
-  driver.drop_index()
-  driver.create_index()
-  driver.reindex(db_session)
+    driver = get_driver()
+    if driver is False:
+        return
+    driver.drop_index()
+    driver.create_index()
+    driver.reindex(db_session)
+
 
 def search(doc_type, query):
-  driver = get_driver()
-  if driver is False:
-    return
-  return driver.search(doc_type, query)
+    driver = get_driver()
+    if driver is False:
+        return
+    return driver.search(doc_type, query)
